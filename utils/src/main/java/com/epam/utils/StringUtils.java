8b626ea5dd7c735f09364e7f15cc8a651364fcab
Jurijs <j.lacis.code@gmail.com>
2024-01-08 13:04:56 +0800

    changed parseInt to parseDouble

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,6 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return org.apache.commons.lang3.math.NumberUtils.isParsable(str) && Integer.parseInt(str) > 0;
+        return org.apache.commons.lang3.math.NumberUtils.isParsable(str) && Double.parseDouble(str) > 0;
     }
 }
